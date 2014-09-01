f9cf7b37c30fab0a71793354a4f3dd41a64d8e5f
Cathy + Qian <cnangini@gmail.com>
2014-09-01 18:34:09 +0300

    WIP: url now displays id but visiting url fn needs to be created

--- app/controllers/issues/issue.js
+++ app/controllers/issues/issue.js
@@ -1,12 +1,18 @@
 import Ember from 'ember';
 
 var IssueController = Ember.ObjectController.extend({
+  setupController: function(controller, model) {
+    controller.set('model', issue);
+  },
 
   actions: {
     toggleExpand: function() {
       this.toggleProperty('isExpanded');
 
-      //this.transitionToRoute('/issues/' + issue_id)
+      this.transitionToRoute('/issues/' + this.get('id'));
+     // debugger;
+
+      //this.transitionToRoute('/issues/' + issue.id)
       
      //  var issue = issuesContent.issue;  //this.modelFor('issue');
      //  var issueUrl = issue.get('url');
@@ -15,7 +21,12 @@ var IssueController = Ember.ObjectController.extend({
     }
   },
 
-  isExpanded: false
+  isExpanded: false,
+
+  geturl: function() {
+    console.log('url', this.get('router.url'))
+  },
+
 
   //   // If our current issue is changed, notify the router
   // _currentIssueChanged: function() {
--- app/routes/issues/issue.js
+++ app/routes/issues/issue.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 var IssueRoute = Ember.Route.extend({
+  
+  
 
   // model: function(params) {
   //   return this.store.find('issue', params.issue_id);
