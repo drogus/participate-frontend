9846d831858535a3264c6cd48cd99df9d5d82f79
Piotr Sarnacki <drogus@gmail.com>
2014-09-18 15:52:09 +0200

    Close issue after it's clicked second time

--- app/routes/issues/index.js
+++ app/routes/issues/index.js
@@ -1,4 +1,7 @@
 var IssuesIndexRoute = Ember.Route.extend({
+  setupController: function(controller, model) {
+    this.controllerFor('issues').set('expandedIssueId', null);
+  }
 });
 
 export default IssuesIndexRoute;
